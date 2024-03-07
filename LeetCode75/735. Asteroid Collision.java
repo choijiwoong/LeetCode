dd8dde3c0b7f65e70cb97bb3c32e5dbb059fd917
최지웅 <gogogi313@naver.com>
2024-03-08 01:02:13 +0900

    feat. testcase 184까지 pass

--- LeetCode75/735. Asteroid Collision.java
+++ LeetCode75/735. Asteroid Collision.java
@@ -5,35 +5,65 @@ class Solution {
         //1. 스택
         Stack<Integer> stack=new Stack<>();
 
-        //2. 로직 시작
-        for(int i=0; i<size; i++){
-            int current_num=asteroids[i];
-            if(current_num>0){
-                stack.push(current_num);
-            } else{//3. 음수 발견 시 충돌발생
-                //반복문을 사용하기 이전, 종료조건 설정_양수의 절댓값이 더 큰 경우.
-                //같은 경우 둘 다 사라지며, 음수의 절댓값이 더 큰 경우 스택에서 원소를 추가적으로 꺼낸다.
-                int abs_current_num=current_num*-1;//4. 만난 음수의 절댓값
-                for(int poped_num=stack.pop(); poped_num>abs_current_num; poped_num=stack.pop()){
-                    if(poped_num==abs_current_num){
-                        break;
-                    } else if(poped_num<abs_current_num){
+        //1-2. 예외처리, 이미 완료된 경우 testcase 153
+        int count=0;
+        for(int i=0; i<size-1; i++){
+            int cur=asteroids[i];
+            int next=asteroids[i+1];
+            if(cur*next<0){
+                count++;
+            }
+        }
+        if(count==1&& asteroids[0]<0)
+            return asteroids;
 
-                    } else{
-                        stack.push(poped_num);
-                        break;
+        //2. 로직 시작
+        for(int i=0; i<size; i++) {
+            //2-1. 해당하는 값 읽기
+            int current_num = asteroids[i];
+
+            //2-2. 스택이 비어있다면 별도의 확인 없이 푸시
+            if (stack.isEmpty()) {
+                stack.push(current_num);
+                continue;
+            }
+
+            //2-3. 스택의 값 부호와 현재의 값 부호가 같다면 푸시
+            if (current_num * stack.peek() > 0) {
+                stack.push(current_num);
+                continue;
+            } else {//만약 부호가 다르다면
+                int pop_num = stack.pop();
+                int abs_current_num = Math.abs(current_num);
+                int abs_pop_num = Math.abs(pop_num);
+                //2-4. 기존의 값이 더 크다면 무시하고 다시 원래의 값 스택에 복구(푸시)
+                if (abs_pop_num > abs_current_num) {
+                    stack.push(pop_num);
+                    continue;
+                } else if (abs_pop_num < abs_current_num) {//2-5. 기존의 값이 더 작다면 이길 때 까지 pop
+                    for (; Math.abs(pop_num) < abs_current_num && !stack.isEmpty(); pop_num = stack.pop()) {
                     }
+                    //2-6. 결국 졌다면 current_num을 푸시
+                    if (Math.abs(pop_num)<abs_current_num) {
+                        stack.push(current_num);
+                        continue;
+                    } else {//2-7. 결국 이겼다면 다시 원래의 값 스택에 복구(푸시)
+                        stack.push(pop_num);
+                    }
+                } else {//2-8. 두 수의 크기가 같다면 둘 다 삭제(pop값을 다시 넣지 않음)
+                    continue;
                 }
             }
         }
 
-        //5. 반환 타입으로 변환
+        //3. 반환 타입으로 변환
         int stack_size=stack.size();
         int[] result=new int[stack_size];
 
         for(int i=0; i<stack_size; i++){
             result[stack_size-i-1]=stack.pop();
         }
+
 
         return result;
     }
