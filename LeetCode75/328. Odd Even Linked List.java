087b9a8a1c29e29df612a5d48b087856fea674ce
최지웅 <gogogi313@naver.com>
2024-03-24 22:26:37 +0900

    debug. 홀수번째 원소를 연결리스트 끝으로 옮기기

--- LeetCode75/328. Odd Even Linked List.java
+++ LeetCode75/328. Odd Even Linked List.java
@@ -14,39 +14,49 @@ class Solution {
             시간복잡도 O(n), 공간복잡도 O(1)로 홀수번째 node를 뒤로 link
          */
         //1. 예외처리(head가 비어있거나 하나일 경우)
-        if(head==null){
+        if (head == null) {
             return null;
-        } else if(head.next==null){
+        } else if (head.next == null) {
             return head;
         }
 
         //2. 전체 size 계산
-        int size=1;
-        for(ListNode i=head; i.next!=null; i=i.next){
+        int size = 1;
+        for (ListNode i = head; i.next != null; i = i.next) {
             size++;
         }
 
-        ListNode head2=null, iter2=null, iter=head, prev=head;
-        for(int i=0; i<size; i++){
-            if(i==5)
+        ListNode head2 = null, iter2 = null, iter = head, prev = head;
+        for (int i = 0; i < size; i++) {
+            if (i == 5)
                 System.out.println();
-            if(i==0){
-                iter=iter.next;
-            } else if(i==1){
-                head2=new ListNode(iter.val);
-                iter2=head2;
-                iter=iter.next;
-                prev.next=iter;
-            } else if(i==size-1){//merge
-                iter.next=head2;
-            } else if(i%2==0){
-                prev=iter;
-                iter=iter.next;
-            } else if(i%2==1){
-                iter2.next=new ListNode(iter.val);
-                iter2=iter2.next;
-                iter=iter.next;
-                prev.next=iter;
+            if (i == 0) {
+                iter = iter.next;
+            } else if (i == 1) {
+                if(iter.next!=null) {
+                    head2 = new ListNode(iter.val);
+                    iter2 = head2;
+                    iter = iter.next;
+                    prev.next = iter;
+                } else{
+                    break;
+                }
+            } else if (i == size - 1) {//merge
+                if (i % 2 == 1) {
+                    iter2.next = new ListNode(iter.val);
+                    iter2=iter2.next;
+                    prev.next=head2;
+                } else {
+                    iter.next = head2;
+                }
+            } else if (i % 2 == 0) {
+                prev = iter;
+                iter = iter.next;
+            } else if (i % 2 == 1) {
+                iter2.next = new ListNode(iter.val);
+                iter2 = iter2.next;
+                iter = iter.next;
+                prev.next = iter;
             }//공통코드는 없다.
         }//endfor
 
