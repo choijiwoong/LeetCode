f99c814d236ead65dddd5e8c1c7f9c427dd449a0
최지웅 <gogogi313@naver.com>
2024-04-04 15:40:42 +0900

    debug. 이진트리에서 수직합이 target과 같은경우 세기

--- LeetCode75/437. Path Sum III.java
+++ LeetCode75/437. Path Sum III.java
@@ -15,28 +15,43 @@
  */
 class Solution {
     private int hit_count=0;
-    private int target_sum;
+    private long target_sum;
+    private Queue<TreeNode> queue=new LinkedList();
 
-    private void DFS(TreeNode node, int current_sum){
-        if(node==null){
+    private void DFS(TreeNode node, long current_sum){
+        if(node==null)
             return;
-        }
+        if(node.left!=null)
+            queue.add(node.left);
+        if(node.right!=null)
+            queue.add(node.right);
         
         current_sum+=node.val;
-        if(current_sum==target_sum)
+        if(current_sum==this.target_sum)
             this.hit_count++;
+
         DFS(node.left, current_sum);
         DFS(node.right, current_sum);
-        if(current_sum!=0){
-            DFS(node.left, 0);
-            DFS(node.right, 0);
-        }
+    }
+    private void DFS2(TreeNode node, long current_sum){
+        if(node==null)
+            return;
+
+        current_sum+=node.val;
+        if(current_sum==this.target_sum)
+            this.hit_count++;
         
+        DFS2(node.left, current_sum);
+        DFS2(node.right, current_sum);
     }
 
-    public int pathSum(TreeNode root, int targetSum) {
+    public int pathSum(TreeNode root, long targetSum) {
         this.target_sum=targetSum;
         this.DFS(root, 0);
+        while(!queue.isEmpty()){
+            this.DFS2(queue.remove(), 0);
+        }
+
         return this.hit_count;
     }
 }
