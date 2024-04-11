b799ba3a0da720507f09255ec9a0853d5c8df35b
최지웅 <gogogi313@naver.com>
2024-04-12 01:46:16 +0900

    debug. 이진트리 각 깊이 별 최우측 노드값을의 리스트 반환하기

--- LeetCode75/199. Binary Tree Right Side View.java
+++ LeetCode75/199. Binary Tree Right Side View.java
@@ -1,12 +1,22 @@
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> list=new ArrayList();
-        
-        while(root!=null){
-            list.add(root.val);
-            root=root.right;
+    private List<Integer> list=new ArrayList();
+    private int diter=0;
+    
+    private void DFS(TreeNode node, int depth){
+        if(node==null)
+            return;
+        if(depth==diter){
+            diter++;
+            list.add(node.val);
         }
+        DFS(node.right, depth+1);
+        DFS(node.left, depth+1);
+    }
 
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root==null)
+            return list;
+        DFS(root, 0);
         return list;
     }
 }
