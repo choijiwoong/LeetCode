db96e0afb8fc71f0dc7cc4c1c1d376c43ada9cb7
최지웅 <gogogi313@naver.com>
2024-05-21 13:42:14 +0900

    refactor. 최적화 수행(queue재사용, 불필요 조건 삭제)

--- LeetCode75/547. Number of Provinces.java
+++ LeetCode75/547. Number of Provinces.java
@@ -3,23 +3,26 @@ class Solution {
         int n=isConnected.length;
         int result=0;
         int[] is_visited=new int[n];
+        Queue<Integer> queue=new LinkedList<>();
+        int city_num;
         for(int i=0; i<n; i++){//비교원소1
             if(is_visited[i]==0){//방문을 안한 비교원소1을 찾았다면
                 is_visited[i]=1;//방문처리를 하고
                 result++;//그룹을 새로 추가한다
-                Queue<Integer> queue=new LinkedList<>();
                 queue.add(i);
                 while(!queue.isEmpty()){
-                    int city_num=queue.remove();
+                    city_num=queue.remove();
                     for(int j=0; j<n; j++) {//비교원소2
-                        if (isConnected[city_num][j] == 1 && j!=i) {//비교원소 1과 2가 연결되어 있다면
-                            if(!queue.contains(j) && is_visited[j]==0)
+                        if (isConnected[city_num][j] == 1) {//비교원소 1과 2가 연결되어 있다면
+                            if(is_visited[j]==0){
                                 queue.add(j);
-                            is_visited[j] = 1;//j의 방문처리도 수행한다.
+                                is_visited[j] = 1;//j의 방문처리도 수행한다.
+                            }
                         }
                     }
                 }
             }
+            queue.clear();
         }
 
         return result;
