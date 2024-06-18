59230a56a812d764842bfd3978cd3d5c86dd4015
최지웅 <gogogi313@naver.com>
2024-06-18 14:28:01 +0900

    feat. MLE디버깅 완료, TLE디버깅 필요

--- LeetCode75/1466. Reorder Routes to Make All Paths Lead to the City Zero.java
+++ LeetCode75/1466. Reorder Routes to Make All Paths Lead to the City Zero.java
@@ -1,30 +1,4 @@
 class Solution {
-    private int[][] relations;
-    private int size;
-    private int[] visited;
-    private int change_count=0;
-
-    private void traversal(int node){
-        //탈출조건
-        if(visited[node]==1)
-            return;
-
-        //현재노드 방문처리
-        visited[node]=1;
-        for(int i=0; i<size; i++){
-            if(relations[node][i]==1){//node->i로 접근이 가능한 경우
-                //방향전환
-                change_count++;
-                traversal(i);
-            }
-            
-            if(relations[i][node]==1){//i->node로 접근이 가능한 경우
-                relations[i][node]=0;
-                relations[node][i]=1;
-                traversal(i);
-            }
-        }
-    }
 
     public int minReorder(int n, int[][] connections) {
         //노드의 수 구하기
@@ -35,17 +9,30 @@ class Solution {
             if(max_node<connections[i][1])
                 max_node=connections[i][1];
         }
-        this.size=max_node+1;
-        
-        //필요한 배열 초기화
-        this.relations=new int[size][size];
-        this.visited=new int[size];
 
-        //관계
-        for (int[] elem : connections)
-            relations[elem[0]][elem[1]]=1;
-        
-        traversal(0);
-        return change_count;
+        int result=0;
+        Queue<Integer> queue=new LinkedList<Integer>();
+        int[] visited=new int[max_node+1];
+
+        queue.add(0);
+        while(!queue.isEmpty()){
+            int node=queue.remove();
+            if(visited[node]==1)
+                continue;
+            visited[node]=1;
+            for(int[] conn : connections){
+                if(conn[0]==node){
+                    if(visited[conn[1]]==1)
+                        continue;
+                    result++;
+                    queue.add(conn[1]);
+                } else if(conn[1]==node){
+                    if(visited[conn[0]]==1)
+                        continue;
+                    queue.add(conn[0]);
+                }
+            }
+        }
+        return result;
     }
 }
