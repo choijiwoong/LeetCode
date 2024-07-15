bbb9bb785087df15db44ed418690c3a03a757524
최지웅 <gogogi313@naver.com>
2024-07-15 21:21:42 +0900

    feat. 배열로 전부 바꾸어보았지만,, 여전히 TLE 해결 실패

--- LeetCode75/1466. Reorder Routes to Make All Paths Lead to the City Zero.java
+++ LeetCode75/1466. Reorder Routes to Make All Paths Lead to the City Zero.java
@@ -1,42 +1,34 @@
 class Solution {
     public int minReorder(int n, int[][] connections) {//n은 노드의 개수, connections는 a->b방향관계
-        //connections를 List형태로 순서를 유지하며 저장한다.
-        List<Integer> queue=new ArrayList<>();//연결관계 저장소
-        for(int[] conn : connections){//모든 연결관계를 순회하면서 구조 재정립
-            queue.add(conn[0]);
-            queue.add(conn[1]);
+        int result=0, idx=-1, end=0, start=0;
+        int index, target_index, target_val, i;
+
+        int[] indexes=new int[connections.length*2];
+        int[] list=new int[connections.length*2];
+        for(int[] conn: connections){
+            list[++idx]=conn[0];
+            if(conn[0]==0)
+                indexes[end++]=idx;
+
+            list[++idx]=conn[1];
+            if(conn[1]==0)
+                indexes[end++]=idx;
         }
 
-        //DFS를 위해 도시(0)이 위치하는 인덱스 정보를 큐에 넣어 준비를 한다.(초기값 세팅)
-        int result=0;
-        Queue<Integer> indexes=new LinkedList<>();//접근할 인덱스의 목록
-        for(int i=0; i<queue.size(); i++)
-            if(queue.get(i)==0)
-                indexes.add(i);
-
         //0부터 시작해서 DFS진행
-        while(!indexes.isEmpty()){
-            int index=indexes.remove();
+        while(start<end){
+            index=indexes[start++];
             if(index%2==0) {//짝수라면 시작지점
                 result++;
-                int target_index=index+1;
-                int target_val=queue.get(target_index);
-                queue.set(target_index, -1);//현재 index와 같이 처리한 쌍의 데이터 무시를 위함
-                for(int i=0; i<queue.size(); i++){
-                    if(queue.get(i)==target_val){
-                        indexes.add(i);
-                    }
-                }
+                target_index=index+1;
             } else{//홀수라면 도착지점
-                int target_index=index-1;
-                int target_val=queue.get(target_index);
-                queue.set(target_index, -1);//현재 index와 같이 처리한 쌍의 데이터 무시를 위함
-                for(int i=0; i<queue.size(); i++){
-                    if(queue.get(i)==target_val){
-                        indexes.add(i);
-                    }
-                }
+                target_index=index-1;
             }
+            target_val=list[target_index];
+            list[target_index]=-1;//현재 index와 같이 처리한 쌍의 데이터 무시를 위함
+            for(i=0; i<list.length; i++)
+                if(list[i]==target_val)
+                    indexes[end++]=i;
         }
 
         return result;
