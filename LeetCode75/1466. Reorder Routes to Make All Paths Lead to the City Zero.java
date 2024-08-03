628e43b03181bba609562575405cde2fa3a6eca7
최지웅 <gogogi313@naver.com>
2024-08-03 15:43:05 +0900

    feat. 다리가 놓여진 지역에서 도시까지 접근하기 위한 최소 다리방향전환 횟수

--- LeetCode75/1466. Reorder Routes to Make All Paths Lead to the City Zero.java
+++ LeetCode75/1466. Reorder Routes to Make All Paths Lead to the City Zero.java
@@ -1,34 +1,46 @@
 class Solution {
-    public int minReorder(int n, int[][] connections) {//n은 노드의 개수, connections는 a->b방향관계
-        int result=0, idx=-1, end=0, start=0;
-        int index, target_index, target_val, i;
+    public static int minReorder(int n, int[][] connections) {
+        int result = 0;
+        List<List<Integer>> adj = new ArrayList<>(n);
+        List<List<Integer>> reverseAdj = new ArrayList<>(n);
 
-        int[] indexes=new int[connections.length*2];
-        int[] list=new int[connections.length*2];
-        for(int[] conn: connections){
-            list[++idx]=conn[0];
-            if(conn[0]==0)
-                indexes[end++]=idx;
-
-            list[++idx]=conn[1];
-            if(conn[1]==0)
-                indexes[end++]=idx;
+        // 인접 리스트 초기화
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+            reverseAdj.add(new ArrayList<>());
         }
 
-        //0부터 시작해서 DFS진행
-        while(start<end){
-            index=indexes[start++];
-            if(index%2==0) {//짝수라면 시작지점
-                result++;
-                target_index=index+1;
-            } else{//홀수라면 도착지점
-                target_index=index-1;
+        // 인접 리스트와 역방향 인접 리스트 구성
+        for (int[] conn : connections) {
+            adj.get(conn[0]).add(conn[1]);
+            reverseAdj.get(conn[1]).add(conn[0]);
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n];
+        queue.add(0);
+        visited[0] = true;
+
+        // BFS를 통해 모든 노드 방문
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            
+            // 정방향 인접 리스트 탐색
+            for (int neighbor : adj.get(node)) {
+                if (!visited[neighbor]) {
+                    result++;
+                    queue.add(neighbor);
+                    visited[neighbor] = true;
+                }
             }
-            target_val=list[target_index];
-            list[target_index]=-1;//현재 index와 같이 처리한 쌍의 데이터 무시를 위함
-            for(i=0; i<list.length; i++)
-                if(list[i]==target_val)
-                    indexes[end++]=i;
+
+            // 역방향 인접 리스트 탐색
+            for (int neighbor : reverseAdj.get(node)) {
+                if (!visited[neighbor]) {
+                    queue.add(neighbor);
+                    visited[neighbor] = true;
+                }
+            }
         }
 
         return result;
