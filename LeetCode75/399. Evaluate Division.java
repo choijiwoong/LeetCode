e72fb8e52148993e550b9edd692c2764a9b0e0fb
choijiwoong <gogogi313@naver.com>
2024-09-10 15:56:09 +0900

    feat. 연립 제외 단일항 연산 가능

--- LeetCode75/399. Evaluate Division.java
+++ LeetCode75/399. Evaluate Division.java
@@ -1,15 +1,5 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 class Solution {
-    private Map<String, String> db=new HashMap<>();
-
-    private void parse_equation(List<String> equation, double value){
-        if(equation.size()!=2)
-            throw new NumberFormatException("equation의 원소가 2개가 아닙니다.");
-
+    private List<String> abbreviation(List<String> equation){
         StringBuilder eq1=new StringBuilder(equation.get(0));
         StringBuilder eq2=new StringBuilder(equation.get(1));
 
@@ -24,55 +14,36 @@ class Solution {
                 }
             }
         }
-
-        db.put(eq1.toString(), value+eq2.toString());
-        //eq1=value*eq2정보 존재
-    }
-
-    private double calc_query(List<String> query){
-        if(query.size()!=2)
-            throw new NumberFormatException("equation의 원소가 2개가 아닙니다.");
-
-        StringBuilder query1=new StringBuilder(query.get(0));
-        StringBuilder query2 =new StringBuilder(query.get(1));
-
-        //문자약분처리
-        for(int i=0; i<query1.length(); i++){
-            for(int j = 0; j< query2.length(); j++){
-                //문자처리
-                if(query1.substring(i,i+1).equals(query2.substring(j,j+1))){
-                    query1.deleteCharAt(i);
-                    query2.deleteCharAt(j);
-                    i--; j--;//(현재 위치가 사라지로 shift되기에 다시 검사가 필요하기에 --)
-                }
-            }
-        }
-
-        return 0.0;
+        //재활용
+        equation.set(0, eq1.toString());
+        equation.set(1, eq2.toString());
+        return equation;
     }
 
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        double[] result=new double[queries.size()];
+        //equations순회하며 동일한건 제거
+        //그대로 db에 넣으면서 거꾸로 value도 넣음
+        //queries에 db에 있으면 value를 리턴
+        Map<List<String>, Double> db=new HashMap();
 
         for(int i=0; i<equations.size(); i++){
-            parse_equation(equations.get(i), values[i]);
+            List<String> simple_equation=abbreviation(equations.get(i));
+            db.put(simple_equation, values[i]);
+
+            List<String> reverse_equation=new ArrayList<>();
+            reverse_equation.add(simple_equation.get(1));
+            reverse_equation.add(simple_equation.get(0));
+            db.put(reverse_equation, 1/values[i]);
         }
 
+        double[] result=new double[queries.size()];
         for(int i=0; i<queries.size(); i++){
-            calc_query(queries.get(i));
+            if(db.containsKey(queries.get(i)))
+                result[i]=db.get(queries.get(i));
+            else
+                result[i]=-1;
+
         }
-
         return result;
-    }
-
-    public static void main(String[] args){
-        Solution solution=new Solution();
-
-        //example1 준비
-        List<List<String>> equations= new ArrayList<>();
-        double[] values=new double[1];
-        List<List<String>> queries=new ArrayList<>();
-
-        solution.calcEquation(equations, values, queries);
     }
 }
