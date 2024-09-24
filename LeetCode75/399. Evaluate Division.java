467d0fd41b8ca9f7821c067e9518abacb530539e
choijiwoong <gogogi313@naver.com>
2024-09-24 09:04:23 +0900

    refactor. 성능최적화 시간 185->160ms

--- LeetCode75/399. Evaluate Division.java
+++ LeetCode75/399. Evaluate Division.java
@@ -1,22 +1,14 @@
 class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        //equations순회하며 동일한건 제거
-        //그대로 db에 넣으면서 거꾸로 value도 넣음
-        //queries에 db에 있으면 value를 리턴
+        //equation과 대응되는 value를 저장할 index_table
         Map<List<String>, Double> db=new HashMap<>();
-        //기본값 저장
-        List<String> default_data=new ArrayList<>();
-        default_data.add("1"); default_data.add("1");
-        db.put(default_data, 1.0);
+        Set<String> used_string=new HashSet<>();
 
         for(int i=0; i<equations.size(); i++){
-            //equation을 db에 저장
+            //기존의 equation을 db에 저장
             List<String> simple_equation=equations.get(i);
             db.put(simple_equation, values[i]);//equation을 db에 저장
-            List<String> reverse_equation=new ArrayList<>();
-            reverse_equation.add(simple_equation.get(1));
-            reverse_equation.add(simple_equation.get(0));
-            db.put(reverse_equation, 1/values[i]);//역순 equation을 db에 저장
+            used_string.addAll(simple_equation);
         }
 
         //연립을 위해 동일한 항을 가지는 항을 찾아 나머지 두개를 이용하여, 연립 항 정보를 찾아 db에 넣는다.
@@ -26,64 +18,28 @@ class Solution {
                 for (Map.Entry<List<String>, Double> eq2 : db.entrySet()) {
                     if (eq1.equals(eq2))//동일한 값이면 넘어감. 연립할게없음
                         continue;
+
                     List<String> new_equation = new ArrayList<>();
-                    List<String> rev_new_equation = new ArrayList<>();
-                    double new_value = -1.0, rev_new_value = -1.0;
-                    //System.out.println(eq1.getKey().get(0)+", "+eq1.getKey().get(1)+", "+eq2.getKey().get(0)+", "+eq2.getKey().get(1));
+                    double new_value = -1.0;
                     if (eq1.getKey().get(0).equals(eq2.getKey().get(0))) {//case 1
                         new_equation.add(eq2.getKey().get(1));
                         new_equation.add(eq1.getKey().get(1));
-                        if(new_equation.get(0).equals("c") && new_equation.get(1).equals("b"))
-                            System.out.println();
                         new_value = eq1.getValue() / eq2.getValue();
-
-                        rev_new_equation.add(eq1.getKey().get(1));
-                        rev_new_equation.add(eq2.getKey().get(1));
-                        rev_new_value = 1 / new_value;
-
-                        if(!db.containsKey(new_equation) || !db.containsKey(rev_new_equation)) {
-                            new_db.put(new_equation, new_value);
-                            new_db.put(rev_new_equation, rev_new_value);
-                        }
                     } else if (eq1.getKey().get(0).equals(eq2.getKey().get(1))) {//case2
                         new_equation.add(eq2.getKey().get(0));
                         new_equation.add(eq1.getKey().get(1));
                         new_value = eq1.getValue() * eq2.getValue();
-
-                        rev_new_equation.add(eq1.getKey().get(1));
-                        rev_new_equation.add(eq2.getKey().get(0));
-                        rev_new_value = 1 / new_value;
-
-                        if(!db.containsKey(new_equation) || !db.containsKey(rev_new_equation)) {
-                            new_db.put(new_equation, new_value);
-                            new_db.put(rev_new_equation, rev_new_value);
-                        }
                     } else if (eq1.getKey().get(1).equals(eq2.getKey().get(0))) {//case3
                         new_equation.add(eq1.getKey().get(0));
                         new_equation.add(eq2.getKey().get(1));
                         new_value = eq1.getValue() * eq2.getValue();
-
-                        rev_new_equation.add(eq2.getKey().get(1));
-                        rev_new_equation.add(eq1.getKey().get(0));
-                        rev_new_value = 1 / new_value;
-
-                        if(!db.containsKey(new_equation) || !db.containsKey(rev_new_equation)) {
-                            new_db.put(new_equation, new_value);
-                            new_db.put(rev_new_equation, rev_new_value);
-                        }
                     } else if (eq1.getKey().get(1).equals(eq2.getKey().get(1))) {//case4
                         new_equation.add(eq1.getKey().get(0));
                         new_equation.add(eq2.getKey().get(0));
                         new_value = eq1.getValue() / eq2.getValue();
-
-                        rev_new_equation.add(eq2.getKey().get(0));
-                        rev_new_equation.add(eq1.getKey().get(0));
-                        rev_new_value = 1 / new_value;
-
-                        if(!db.containsKey(new_equation) || !db.containsKey(rev_new_equation)) {
-                            new_db.put(new_equation, new_value);
-                            new_db.put(rev_new_equation, rev_new_value);
-                        }
+                    }
+                    if(!db.containsKey(new_equation) && !new_equation.isEmpty()){//위에서 새로운 equation이 만들어졌고, 존재하지 않는 값이라면 추가(이때, 역순의 값이 들어갈 순 있다)
+                        new_db.put(new_equation, new_value);
                     }
                 }
             }
@@ -91,20 +47,27 @@ class Solution {
         }
 
 
-        //System.out.println(db.size());
-        //System.out.println(db.entrySet().size());
+//        System.out.println(db.size());
+//        System.out.println(db.entrySet().size());
 //        for(Map.Entry<List<String>, Double> data: db.entrySet())
 //            System.out.println("key: "+data.getKey().get(0)+", "+data.getKey().get(1)+" / value: "+data.getValue());
 
         double[] result=new double[queries.size()];
         for(int i=0; i<queries.size(); i++){
-            //System.out.println("ab res: "+abbreviation(queries.get(i)));
+            List<String> rev_query=new ArrayList<>();
+            rev_query.add(queries.get(i).get(1));
+            rev_query.add(queries.get(i).get(0));
             //db에 query식이 그대로 들어가 있는지 확인
             if(db.containsKey(queries.get(i))) {//약분한 쿼리를 기반으로 db 서칭. 미지수는 위의 예외처리에서 걸러짐.
                 result[i] = db.get(queries.get(i));
-            } else {
+            } else if(db.containsKey(rev_query)){//역순 확인
+                result[i]=1/db.get(rev_query);
+            } else if(rev_query.get(0).equals(rev_query.get(1)) && used_string.contains(rev_query.get(0))){
+                result[i] = 1;
+            } else{
                 result[i] = -1;
             }
+            System.out.print(result[i]);
         }
 
         return result;
