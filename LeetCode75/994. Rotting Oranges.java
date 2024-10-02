00e54d34e1e2d74956f693f020151014f722cbca
choijiwoong <gogogi313@naver.com>
2024-10-02 10:43:41 +0900

    feat. case 1 통과 초안코드

---
+++ LeetCode75/994. Rotting Oranges.java
@@ -0,0 +1,48 @@
+class Solution {
+    int m, n;
+    public int orangesRotting(int[][] grid) {
+        m=grid.length;
+        n=grid[0].length;
+        int max=Math.max(m, n);
+        Queue<int[]> queue=new LinkedList<>();
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==2){
+                    queue.offer(new int[]{i, j});
+                }
+            }
+        }
+
+        int minute;
+        int[] index;
+        int[][] directions=new int[][]{{-1,0}, {1,0}, {0,-1}, {0,1}};
+        Queue<int[]> buf_queue=new LinkedList<>();
+        for(minute=0; minute<max+1; minute++){//
+            while(!queue.isEmpty()){
+                index=queue.poll();
+
+                for(int[] direction: directions){
+                    int[] new_index=new int[]{index[0]+direction[0], index[1]+direction[1]};
+                    if(is_valid_idx(new_index) && grid[new_index[0]][new_index[1]] == 1) {
+                        grid[new_index[0]][new_index[1]]=2;
+                        buf_queue.add(new_index);
+                    }
+                }
+            }
+            queue.addAll(buf_queue);
+            buf_queue.clear();
+        }
+
+        if(minute==max)
+            return -1;
+
+        return minute;
+    }
+
+    private boolean is_valid_idx(int[] index){
+        if(index[0]<0 || index[0]>=m || index[1]<0 || index[1]>=n)//범위체크 종료조건
+            return false;
+        return true;
+    }
+}
