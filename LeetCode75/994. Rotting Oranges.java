4e530d703fe9d639d5805f4488631abd8175c184
choijiwoong <gogogi313@naver.com>
2024-10-04 16:15:39 +0900

    refactor. minute의 오류 level의 도입으로 수정

--- LeetCode75/994. Rotting Oranges.java
+++ LeetCode75/994. Rotting Oranges.java
@@ -5,52 +5,71 @@ class Solution {
         n=grid[0].length;
         int max=Math.max(m, n);
         Queue<int[]> queue=new LinkedList<>();
+        int[][] grid_buf;
+        int[][] directions=new int[][]{{-1,0}, {1,0}, {0,-1}, {0,1}};
+        int minute;
+        int[] index;
+        boolean is_finish;
+        int min_minute=-1;
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j]==2){
-                    queue.offer(new int[]{i, j});
-                }
-            }
-        }
+                    //얘부터 시작했을 때
+                    queue.add(new int[]{i, j});
+                    grid_buf=new int[grid.length][grid[0].length];
+                    for(int gi=0; gi<m; gi++)
+                        for(int gj=0; gj<n; gj++)
+                            grid_buf[gi][gj]=grid[gi][gj];
+                    is_finish=true;
+                    minute=0;
 
-        int minute;
-        int[] index;
-        int[][] directions=new int[][]{{-1,0}, {1,0}, {0,-1}, {0,1}};
-        Queue<int[]> buf_queue=new LinkedList<>();
-        for(minute=0; minute<max+1; minute++){//
-            while(!queue.isEmpty()){
-                index=queue.poll();
+                    int level=queue.size();
+                    while(!queue.isEmpty()){//하나씩 오염시킬 사과의 위치가 담김
+                        index=queue.poll();//인덱스를 가져와서
+                        for(int[] direction: directions){//사 방향에 대해 유효한 인덱스고 신선한 사과가 있을 때
+                            int[] new_index=new int[]{index[0]+direction[0], index[1]+direction[1]};
+                            if(is_valid_idx(new_index) && grid_buf[new_index[0]][new_index[1]] == 1) {
+                                grid_buf[new_index[0]][new_index[1]]=2;//오염시킨다.
+                                queue.add(new_index);//감염된 사과의 위치를 추가함
+                            }
+                        }
+                        level--;
+                        if(level==0) {
+                            minute++;
+                            level = queue.size();
+                        }
+                    }
+                    //더이상 오염시킬 사과가 없다면(큐는 비어있는 상태)
+                    //성공적으로 모든 사과가 감염되었는지 확인
+                    for(int ii=0; ii<m; ii++) {
+                        for (int jj = 0; jj < n; jj++) {
+                            if (grid_buf[ii][jj] == 1) {
+                                is_finish = false;
+                                break;
+                            }
+                        }
+                        if(!is_finish)
+                            break;
+                    }
 
-                for(int[] direction: directions){
-                    int[] new_index=new int[]{index[0]+direction[0], index[1]+direction[1]};
-                    if(is_valid_idx(new_index) && grid[new_index[0]][new_index[1]] == 1) {
-                        grid[new_index[0]][new_index[1]]=2;
-                        buf_queue.add(new_index);
+                    if(is_finish){//전부 감염 성공
+                        minute--;
+                        if(min_minute==-1) {
+                            min_minute=minute;
+                        } else if(min_minute>minute){//최단 시간 갱신
+                            min_minute=minute;
+                        }
                     }
                 }
             }
-            queue.addAll(buf_queue);
-            buf_queue.clear();
-
-            if(queue.isEmpty())
-                break;
         }
 
-        if(minute==max)
-            return -1;
-
-        for(int i=0; i<m; i++)
-            for(int j=0; j<n; j++)
-                if(grid[i][j]==1)
-                    return -1;
-
-        return minute;
+        return min_minute;
     }
 
     private boolean is_valid_idx(int[] index){
-        if(index[0]<0 || index[0]>=m || index[1]<0 || index[1]>=n)//범위체크 종료조건
-            return false;
-        return true;
+        //범위체크 종료조건
+        return index[0] >= 0 && index[0] < m && index[1] >= 0 && index[1] < n;
     }
 }
