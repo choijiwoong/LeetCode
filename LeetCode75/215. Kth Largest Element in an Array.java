28c47f2c27deb8a713dba0a50587ca69dd5b7ee6
choijiwoong <gogogi313@naver.com>
2024-10-10 13:06:00 +0900

    refactor. 배열에서 n번째 큰 수 리턴(sort->PriorityQueue)

--- LeetCode75/215. Kth Largest Element in an Array.java
+++ LeetCode75/215. Kth Largest Element in an Array.java
@@ -1,5 +1,10 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        return Arrays.stream(nums).sorted().toArray()[nums.length-k];
+        PriorityQueue<Integer> queue=new PriorityQueue<>();
+        for (int num : nums)
+            queue.add(num);
+        for(int i=0; i<nums.length-k; i++)
+            queue.poll();
+        return queue.poll();
     }
 }
