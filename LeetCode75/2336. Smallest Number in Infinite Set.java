3266a6e4b3cf4f63ba9bc54e58c4970384fd99e2
choijiwoong <gogogi313@naver.com>
2024-10-11 13:26:27 +0900

    feat. 무한 양의정수 클래스 구현하기

---
+++ LeetCode75/2336. Smallest Number in Infinite Set.java
@@ -0,0 +1,26 @@
+class SmallestInfiniteSet {
+    PriorityQueue<Integer> queue;
+
+    public SmallestInfiniteSet() {
+        queue=new PriorityQueue<Integer>();
+        for(int i=1; i<1000; i++){
+            queue.add(i);
+        }
+    }
+
+    public int popSmallest() {
+        return queue.remove();
+    }
+
+    public void addBack(int num) {
+        if(!queue.contains(num))
+            queue.add(num);
+    }
+}
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet obj = new SmallestInfiniteSet();
+ * int param_1 = obj.popSmallest();
+ * obj.addBack(num);
+ */
