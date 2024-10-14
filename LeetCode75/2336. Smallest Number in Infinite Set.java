41f0b6c9e5b92b16210873af920cc99064e4f214
choijiwoong <gogogi313@naver.com>
2024-10-14 13:52:46 +0900

    refactor. 315ms로 최적화 필요

--- LeetCode75/2336. Smallest Number in Infinite Set.java
+++ LeetCode75/2336. Smallest Number in Infinite Set.java
@@ -1,25 +1,32 @@
 class SmallestInfiniteSet {
     PriorityQueue<Integer> queue;
-
+    int min_val=1;
+    
     public SmallestInfiniteSet() {
         queue=new PriorityQueue<Integer>();
     }
 
     public int popSmallest() {
-        int min=1;
+        int min=min_val;
         if(!queue.contains(min)){//가용한 값이라면
             queue.add(min);
+            min_val=findNextMin(min);
             return min;
         } else {//가용하지 않은 값이라면
-            while(queue.contains(++min)){}//가용한 값이 나올때까지++
-            queue.add(min);
-            return min;
+            return -1;
         }
+    }
+
+    private int findNextMin(int min){
+        while(queue.contains(++min)){}
+        return min;
     }
 
     public void addBack(int num) {
         if(queue.contains(num))
             queue.remove(num);
+        if(min_val>num)
+            min_val=num;
     }
 }
 
