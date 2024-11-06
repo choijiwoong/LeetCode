5bc508f4a2356de0d58827f0a0e1fa0f8a26900f
최지웅 <gogogi313@naver.com>
2024-11-06 12:57:30 +0900

    debug. sort를 이용한 접근_디버깅 필요

--- LeetCode75/2542. Maximum Subsequence Score.java
+++ LeetCode75/2542. Maximum Subsequence Score.java
@@ -1,30 +1,47 @@
 class Solution {
+    private int[] sort_with_index(int[] nums){
+        Integer[] indices=new Integer[nums.length];//정렬 시 변경될 인덱스 정보를 저장할 인덱스 lookup table
+        for(int i=0; i<nums.length; i++)
+            indices[i]=i;//초기값 설정
+
+        Arrays.sort(indices, new Comparator<Integer>(){//Arrays.sort에서 특별한 조검을 지정하고 싶으면 Comparator객체를 생성해서 준다.
+            @Override
+            public int compare(Integer i1, Integer i2){//compare함수를 가지고 있으면 되고 반환타입읜 Comparator<>제네릭 타입을 따른다.
+                return Integer.compare(nums[i1], nums[i2]);//num1-num2보다 직관적으로 Integer.compare을 사용.
+            }
+        });
+
+        Arrays.sort(nums);//실제 배열 정렬
+
+        return Arrays.stream(indices).mapToInt(Integer::intValue).toArray();//Integer[]타입을 int[]로 변경
+    }
     public long maxScore(int[] nums1, int[] nums2, int k) {
         int length=nums1.length;
-        int[] pointer=new int[k];//동적 할당 사용
-        for(int i=0; i<k; i++)
-            pointer[i]=i;//초기값 세팅(시작 index)
-        
-        int minimum_score=10000000;
-        for(int ptr=k-1; 0<=ptr; ptr--){//끝 원소부터 역으로 시작
-            for(;pointer[ptr]<length; pointer[ptr]++){
-                int sum=0, min=1001;
-                for(int i=0; i<k; i++){
-                    sum+=nums1[pointer[i]];
-                    if(min>nums1[pointer[i]]){
-                        min=nums1[pointer[i]];
-                    }
+        int max_score=0;
+
+        int[] sorted_nums1_indices=sort_with_index(nums1);
+        int[] sorted_nums2_indices=sort_with_index(nums2);
+        for(int i=length-1; k<=i; i--){
+            int sum=0, min=100000;//데이터가 양의 정수기에 0으로 초기화
+            boolean is_align=true;//인덱스 순서 체크 플래그
+            for(int j=0; j<k; j++){
+                //가산
+                sum+=nums1[i-j];
+                //최소
+                if(min>nums2[i-j]) {
+                    min = nums2[i - j];
                 }
-                int score=sum*min; System.out.println(score);
-                if(minimum_score>score){
-                    minimum_score=score;
+                //인덱스 순서 체크
+                if(sorted_nums1_indices[i-j] == sorted_nums2_indices[i-j]) {
+                    is_align = false;
+                    break;
                 }
             }
-            if(ptr-1!=-1)
-                pointer[ptr-1]++;
-            pointer[ptr]=k-1;
+            if(is_align && max_score < sum*min){
+                max_score=sum*min;
+            }
         }
 
-        return minimum_score;
+        return max_score;
     }
 }
