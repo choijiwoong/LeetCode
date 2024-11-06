c7ad8035091bc091aa33ea395b8d7187f24fe5f3
최지웅 <gogogi313@naver.com>
2024-11-06 13:34:02 +0900

    debug. hint 3개 참고 15/28

--- LeetCode75/2542. Maximum Subsequence Score.java
+++ LeetCode75/2542. Maximum Subsequence Score.java
@@ -17,31 +17,13 @@ class Solution {
     }
     public long maxScore(int[] nums1, int[] nums2, int k) {
         int length=nums1.length;
-        int max_score=0;
-
-        int[] sorted_nums1_indices=sort_with_index(nums1);
+        
         int[] sorted_nums2_indices=sort_with_index(nums2);
-        for(int i=length-1; k<=i; i--){
-            int sum=0, min=100000;//데이터가 양의 정수기에 0으로 초기화
-            boolean is_align=true;//인덱스 순서 체크 플래그
-            for(int j=0; j<k; j++){
-                //가산
-                sum+=nums1[i-j];
-                //최소
-                if(min>nums2[i-j]) {
-                    min = nums2[i - j];
-                }
-                //인덱스 순서 체크
-                if(sorted_nums1_indices[i-j] == sorted_nums2_indices[i-j]) {
-                    is_align = false;
-                    break;
-                }
-            }
-            if(is_align && max_score < sum*min){
-                max_score=sum*min;
-            }
-        }
 
-        return max_score;
+        int min=nums2[length-k], sum=0;
+        for(int i=0; i<k; i++){
+            sum+=nums1[sorted_nums2_indices[length-1-i]];
+        }
+        return min*sum;
     }
 }
