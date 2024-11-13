85942f0fc6d835f798b3fde1b224688f24cedb72
최지웅 <gogogi313@naver.com>
2024-11-13 14:13:31 +0900

    feat. 인덱스 갱신 오류 수정. 11/28

--- LeetCode75/2542. Maximum Subsequence Score.java
+++ LeetCode75/2542. Maximum Subsequence Score.java
@@ -25,22 +25,23 @@ class Solution {
             for(int j=indexes[cur_idx]; j<length; j++){//4. 마지막 인덱스부터 ~ 끝 접근
                 indexes[cur_idx]=j;//5. 함수 전달을 위해 indexes 갱신
                 int result_val=calculate(nums1, nums2, indexes);//6. indexes기반 값 계산
+                //print_indexes(indexes);
                 if(result_val>max_val){//7. 최댓값 갱신
                     max_val=result_val;
                 }
             }
+
             //8. 다음 탐색을 위한 indexes 갱신. 앞의 indexes[]원소 탐색을 위해 앞 index를 ++, 뒤의 인덱스를 한칸 앞으로 옮겨야 한다.
-            if(cur_idx>0){//0번은 이전 원소가 없기 때문에
-                for(int j=cur_idx-1; j<k; j++){//9. 현재 탐색한 인덱스 앞 인덱스를 현재 인덱스로 옮기고 나머지는 +1한 값으로 갱신
+            if(cur_idx-i-1>=0){//0번은 이전 원소가 없기 때문에
+                for(int j=cur_idx-i-1; j<k; j++){//9. 현재 탐색한 인덱스 앞 인덱스를 현재 인덱스로 옮기고 나머지는 +1한 값으로 갱신
                     indexes[j]=j+1;//초기값에서 한칸씩 shift하여 갱신
                 }
             } else{//0번 원소라면
-                for(int j=cur_idx; j<k; j++){//자기부터 shift하여 갱신
+                for(int j=cur_idx-i; j<k; j++){//자기부터 shift하여 갱신
                     indexes[j]=j+1;
                 }
             }
         }
-
         return max_val;
     }
 }
