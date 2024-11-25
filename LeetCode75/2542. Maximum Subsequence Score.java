576c5114750ae1719d884fb1a6741cd189b5f83c
최지웅 <gogogi313@naver.com>
2024-11-25 13:01:38 +0900

    refactor. nums2 최솟값 + 조합 백트래킹. TLE

--- LeetCode75/2542. Maximum Subsequence Score.java
+++ LeetCode75/2542. Maximum Subsequence Score.java
@@ -27,10 +27,26 @@ private int[] nums1, nums2;
 
     public long maxScore(int[] nums1, int[] nums2, int k) {
         final int length=nums1.length;
+        int max_score=0;
 
-        //1. Combination에 사용할 index 리스트 초기화
-        int[] indexes=IntStream.range(0, length).toArray();
-        //2. backtracking
-        return backtrack(nums1, nums2, indexes, k, 0, new ArrayList<>());
+        //1. nums2 정렬 및 nums2 기준 nums1 정렬. nums2에서 최솟값을 기준으로 연산하기 위함
+        int[][] sorted_nums2=IntStream.range(0, length)
+                .mapToObj(i->new int[]{nums2[i], i})
+                .sorted(Comparator.comparingInt(a->a[0]))
+                .toArray(int[][]::new);//[i][0]에 정렬된 값을, [i][1]에 그 값의 원래 인덱스를 저장
+        int[] sorted_nums1=new int[length];
+        for(int i=0; i<length; i++)
+            sorted_nums1[i]=nums1[sorted_nums2[i][1]];//nums1[i][1] 기준으로 nums2 정렬
+
+        //2. 최솟값 후보 선정
+        for(int i=0; i<=length-k; i++){
+            //i+1~length-1까지가 후보군(조합)
+            int[] indexes=IntStream.range(i,length).toArray();//combination을 계산한 nums2기준 index 리스트
+            int score=backtrack(nums1, nums2, indexes, k, 0, new ArrayList<>());//backtracking
+            if(score>max_score)//update max
+                max_score=score;
+        }
+
+        return max_score;
     }
 }
