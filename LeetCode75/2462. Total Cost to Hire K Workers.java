4f2e34dc68c8f221d614e4f447277dc990be3ca6
최지웅 <gogogi313@naver.com>
2024-12-02 14:04:36 +0900

    refactor. linkedlist이용 실제 삭제, TLE발생

--- LeetCode75/2462. Total Cost to Hire K Workers.java
+++ LeetCode75/2462. Total Cost to Hire K Workers.java
@@ -1,27 +1,28 @@
 class Solution {
     public long totalCost(int[] costs, int k, int candidates) {
         final int MAX_COST=10000;
-        final int LENGTH= costs.length;
-        int idx=0, min_cost=MAX_COST, result_cost=0, worker_index=-1;
-        for(int i=0; i<k; i++){// 추출해야하는 워커
+        int LENGTH;
+        int min_cost, result_cost=0, partial_min_index=-1;
+        LinkedList<Integer> data=new LinkedList<>(Arrays.stream(costs).boxed().toList());
+        for(int i=0; i<k; i++){//추출해야하는 워커 별 루프
+            //0~candidates, LENGTH-candidates~LENGTH-1 중에서 최솟값 선정
+            //비용 반영 및 -1화. 만약 bidirectional linked list라면?
+            LENGTH=data.size();
             min_cost=MAX_COST;
-            for(idx=0; idx<LENGTH; idx++){// 탐색
-                int cost=costs[idx];
-                if(cost==-1){
-                    continue;
+            for(int j=0; j<candidates; j++){
+                System.out.printf("size: %d, idx1: %d, idx2: %d\n", data.size(), j, LENGTH-1-j);
+                if(0<=j && j<LENGTH &&data.get(j)<min_cost){
+                    min_cost=data.get(j);
+                    partial_min_index=j;
                 }
-                if(min_cost>cost){// 이미 고른 작업자는 -1로 치환할 것임
-                    min_cost=cost;
-                    worker_index=idx;
+                if(0<=LENGTH-1-j && LENGTH-1-j<LENGTH && data.get(LENGTH-1-j)<min_cost){
+                    min_cost=data.get(LENGTH-1-j);
+                    partial_min_index=LENGTH-1-j;
                 }
-
-                if(LENGTH-candidates*2+1>0 && idx>=candidates){// 다음 간격으로 인덱스 이동
-                    idx+=LENGTH-candidates*2;
-                }
-            }// 현재 세션에서의 작업자 선정 완료
+            }
             result_cost+=min_cost;
-            costs[worker_index]=-1;
-            //System.out.println("result_cost: "+result_cost+", idx= "+worker_index);
+            data.remove(partial_min_index);
+            System.out.println("added "+min_cost+" of "+partial_min_index);
         }
 
         return result_cost;
