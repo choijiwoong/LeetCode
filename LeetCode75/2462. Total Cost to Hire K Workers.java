b4460b7c5a73169a59e50e098336a43b0dac052f
최지웅 <gogogi313@naver.com>
2024-12-03 11:46:16 +0900

    refactor. 배열접근 132/162_OLE 발생

--- LeetCode75/2462. Total Cost to Hire K Workers.java
+++ LeetCode75/2462. Total Cost to Hire K Workers.java
@@ -1,28 +1,51 @@
 class Solution {
     public long totalCost(int[] costs, int k, int candidates) {
         final int MAX_COST=10000;
-        int LENGTH;
-        int min_cost, result_cost=0, partial_min_index=-1;
-        LinkedList<Integer> data=new LinkedList<>(Arrays.stream(costs).boxed().toList());
-        for(int i=0; i<k; i++){//추출해야하는 워커 별 루프
-            //0~candidates, LENGTH-candidates~LENGTH-1 중에서 최솟값 선정
-            //비용 반영 및 -1화. 만약 bidirectional linked list라면?
-            LENGTH=data.size();
+        int LENGTH=costs.length;
+        int min_cost, result_cost=0;
+        int min_index=-1, offset;
+
+        for(int i=0; i<k; i++){
             min_cost=MAX_COST;
-            for(int j=0; j<candidates; j++){
-                System.out.printf("size: %d, idx1: %d, idx2: %d\n", data.size(), j, LENGTH-1-j);
-                if(0<=j && j<LENGTH &&data.get(j)<min_cost){
-                    min_cost=data.get(j);
-                    partial_min_index=j;
+            offset=0;
+            for(int j=0; j<candidates+offset && j<LENGTH; j++){
+                if(costs[j]==-1){
+                    offset++;
+                    continue;
                 }
-                if(0<=LENGTH-1-j && LENGTH-1-j<LENGTH && data.get(LENGTH-1-j)<min_cost){
-                    min_cost=data.get(LENGTH-1-j);
-                    partial_min_index=LENGTH-1-j;
+                if(costs[j]<min_cost){
+                    min_index=j;
+                    min_cost=costs[j];
                 }
             }
+            if(candidates+offset>LENGTH-candidates){//범위가 겹치는가? k고려해야할거같은데 차피 겹치면뭐..
+                for(int j=candidates+offset; j<LENGTH; j++){
+                    if(costs[j]==-1){
+                        offset++;
+                        continue;
+                    }
+                    if(costs[j]<min_cost){
+                        min_index=j;
+                        min_cost=costs[j];
+                    }
+                }
+            } else{//범위가 안겹치는가?
+                offset=0;
+                for(int j=LENGTH-1; j>=LENGTH-candidates+offset; j--){
+                    if(costs[j]==-1){
+                        offset--;
+                        continue;
+                    }
+                    if(costs[j]<min_cost){
+                        min_index=j;
+                        min_cost=costs[j];
+                    }
+                }
+            }
+            min_cost=costs[min_index];
             result_cost+=min_cost;
-            data.remove(partial_min_index);
-            System.out.println("added "+min_cost+" of "+partial_min_index);
+            costs[min_index]=-1;
+            //System.out.println("added "+min_cost+" of "+min_index+" costs: "+Arrays.toString(costs));
         }
 
         return result_cost;
