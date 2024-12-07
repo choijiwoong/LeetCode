19cf1fd08344674daccee67a4b0b613f420a6627
최지웅 <gogogi313@naver.com>
2024-12-07 10:05:44 +0900

    refactor. minHeap초기화 시 left와 right를 두어 새로이 추가할 원소 범위 판단에 사용한다.

--- LeetCode75/2462. Total Cost to Hire K Workers.java
+++ LeetCode75/2462. Total Cost to Hire K Workers.java
@@ -1,43 +1,35 @@
 class Solution {
     public long totalCost(int[] costs, int k, int candidates) {
-        final int MAX_COST=10000;
-        int LENGTH=costs.length;
-        int min_cost, result_cost=0;
-        int min_index=-1, offset;
+        int LENGTH = costs.length;
+        long cost = 0;
 
-        for(int i=0; i<k; i++){
-            min_cost=MAX_COST;
-            offset=0;
-            for(int j=0; j<candidates+offset && j<LENGTH; j++){
-                if(costs[j]==-1){
-                    offset++;
-                    continue;
-                }
-                if(costs[j]<min_cost){
-                    min_index=j;
-                    min_cost=costs[j];
-                }
-            }
+        // MinHeap 초기화
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(
+                (a, b) -> costs[a] != costs[b] ? Integer.compare(costs[a], costs[b]) : Integer.compare(a, b)
+        ); // 비용이 같은 경우 인덱스를 비교하여 앞 쪽 인덱스를 우선으로 둔다.
 
-            if(candidates+offset<=LENGTH-candidates){//범위가 안겹치는가?
-                offset=0;
-                for(int j=LENGTH-1; j>=LENGTH-candidates+offset; j--){
-                    if(costs[j]==-1){
-                        offset--;
-                        continue;
-                    }
-                    if(costs[j]<min_cost){
-                        min_index=j;
-                        min_cost=costs[j];
-                    }
-                }
-            }
-            min_cost=costs[min_index];
-            result_cost+=min_cost;
-            costs[min_index]=-1;
-            //System.out.println("added "+min_cost+" of "+min_index+" costs: "+Arrays.toString(costs));
+        // 두 포인터를 사용해 후보 영역 관리
+        int left = 0, right = LENGTH - 1;
+
+        // 초기 후보 범위를 MinHeap에 추가
+        for (int i = 0; i < candidates; i++) {
+            if (left <= right) minHeap.add(left++);
+            if (left <= right) minHeap.add(right--);
         }
 
-        return result_cost;
+        // k명을 추출하며 비용 계산
+        for (int i = 0; i < k; i++) {
+            int removedIndex = minHeap.poll();
+            cost += costs[removedIndex];
+
+            // 후보 영역에 남은 값 추가
+            if (removedIndex <= left && left <= right) { // 와.. 그냥 left right로 양쪽 candidates 범위 경계를 지정하면 되는구나..
+                minHeap.add(left++);
+            } else if (removedIndex >= right && right >= left) {
+                minHeap.add(right--);
+            }
+        }
+
+        return cost;
     }
 }
