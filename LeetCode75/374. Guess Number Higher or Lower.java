82c21eb639f15724d62e6074cdadde142c8231cc
최지웅 <gogogi313@naver.com>
2024-12-08 15:20:18 +0900

    feat. num범위에 맞추어 Long타입으로 변견

--- LeetCode75/374. Guess Number Higher or Lower.java
+++ LeetCode75/374. Guess Number Higher or Lower.java
@@ -8,21 +8,21 @@
  */
 
 public class Solution extends GuessGame {
-    public int binary_search(int mid, int start, int end){;
-        switch(guess(mid)){
-            case -1:
-                return binary_search((start+mid-1)/2, start, mid-1);
-
-            case 0:
-                break;
-
-            case 1:
-                return binary_search((mid+1+end)/2, mid+1, end);
-
-        }
-        return mid;
-    }
     public int guessNumber(int n) {
-        return binary_search(n/2, 0, n);
+        if(n==1)
+            return 1;
+        long mid=n/2, start=0, end=n, guess_var=guess((int)mid);
+
+        while(guess_var!=0){
+            if(guess_var==1){
+                start=mid+1;
+            } else {//guess_val==-1
+                end=mid-1;
+            }
+            mid=(start+end)/2;
+            guess_var=guess((int)mid);
+            //System.out.println(mid);
+        }
+        return (int)mid;
     }
 }
