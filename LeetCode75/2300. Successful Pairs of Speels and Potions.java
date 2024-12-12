a42566d14901e7082abcd8cc54c0a6a06077bf2f
최지웅 <gogogi313@naver.com>
2024-12-12 10:56:46 +0900

    refactor. potions정렬. 52/56

--- LeetCode75/2300. Successful Pairs of Speels and Potions.java
+++ LeetCode75/2300. Successful Pairs of Speels and Potions.java
@@ -1,12 +1,14 @@
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
         int[] result=new int[spells.length];
-
+        double success_d=success*1.0;
+        Arrays.sort(potions);
         for(int i=0; i<spells.length; i++){
-            double limit=success*1.0/spells[i];
+            double limit=success_d/spells[i];
             for(int j=0; j<potions.length; j++){
                 if(potions[j]>=limit){
-                    result[i]++;
+                    result[i]+=potions.length-j;
+                    break;
                 }
             }
         }
