b5d5ae433937b9cc27b2ce4105f7d7b3f92280f9
최지웅 <gogogi313@naver.com>
2024-12-12 11:24:33 +0900

    feat. 이진탐색으로 같은 값이 아닌 경계면을 찾는 방법(gpt)

--- LeetCode75/2300. Successful Pairs of Speels and Potions.java
+++ LeetCode75/2300. Successful Pairs of Speels and Potions.java
@@ -2,23 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        int[] result=new int[spells.length];
-        double success_d=success*1.0;
+        int[] result = new int[spells.length];
         Arrays.sort(potions);
-        for(int i=0; i<spells.length; i++){
-            int limit= (int) Math.ceil(success_d/spells[i]);
-            int start=0, end=potions.length, mid=(start+end)/2;
-            while(start<=end){
-                if(potions[mid]==limit){
-                    result[i]+=spells.length-mid;
-                    break;
-                } else if(potions[mid]>limit){
-                    end=mid-1;
-                } else{
-                    start=mid+1;
+
+        for (int i = 0; i < spells.length; i++) {
+            long limit = (long) Math.ceil((double) success / spells[i]);
+            int start = 0, end = potions.length;
+        
+            // 이진 탐색으로 lower bound를 찾음
+            while (start < end) {
+                int mid = (start + end) / 2;
+             if (potions[mid] >= limit) {
+                    end = mid; // 조건을 만족하면 왼쪽 영역 탐색
+                } else {
+                    start = mid + 1; // 조건을 만족하지 않으면 오른쪽 영역 탐색
                 }
-                mid=(start+end)/2;
             }
+        
+            // 조건을 만족하는 첫 번째 인덱스(start)부터 끝까지의 개수를 더함
+            result[i] = potions.length - start;
         }
 
         return result;
