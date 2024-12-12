23d64127ad396459a3a4f3288b4ca162c3b3e545
최지웅 <gogogi313@naver.com>
2024-12-12 11:17:37 +0900

    feat. 이진탐색을 이용한 최적화 시도 실패

--- LeetCode75/2300. Successful Pairs of Speels and Potions.java
+++ LeetCode75/2300. Successful Pairs of Speels and Potions.java
@@ -1,15 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
         int[] result=new int[spells.length];
         double success_d=success*1.0;
         Arrays.sort(potions);
         for(int i=0; i<spells.length; i++){
-            double limit=success_d/spells[i];
-            for(int j=0; j<potions.length; j++){
-                if(potions[j]>=limit){
-                    result[i]+=potions.length-j;
+            int limit= (int) Math.ceil(success_d/spells[i]);
+            int start=0, end=potions.length, mid=(start+end)/2;
+            while(start<=end){
+                if(potions[mid]==limit){
+                    result[i]+=spells.length-mid;
                     break;
+                } else if(potions[mid]>limit){
+                    end=mid-1;
+                } else{
+                    start=mid+1;
                 }
+                mid=(start+end)/2;
             }
         }
 
