3c2ea75e569ca6678ed886afdad6af220c68924f
최지웅 <gogogi313@naver.com>
2024-12-13 10:47:33 +0900

    feat. 새로이 이해한 문제를 바탕으로 만든 최선의 코드. fail

--- LeetCode75/162. Find Peak Element.java
+++ LeetCode75/162. Find Peak Element.java
@@ -1,7 +1,14 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        return IntStream.range(0, nums.length)
-                .reduce((a,b)->nums[a]>nums[b]?a:b)
-                .orElse(-1);
+        int start=0, end=nums.length-1, mid=0;//1,2,1
+        while(start<end){
+            mid=(start+end)/2;
+            if(nums[mid+1]>mid){
+                start=mid+1;
+            } else{
+                end=mid-1;
+            }
+        }
+        return mid;
     }
 }
