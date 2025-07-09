5aaec14f3e59f4cd77cf784e2fd3a548396b080e
Choijiwoong <31060798+choijiwoong@users.noreply.github.com>
2025-07-09 18:08:57 +0900

    feat. having이용한 최적화 코드

--- SQL50/570. Managers with at Least 5 Direct Reports.sql
+++ SQL50/570. Managers with at Least 5 Direct Reports.sql
@@ -1,5 +1,10 @@
 SELECT e1.name
-FROM Employee e1
-JOIN Employee e2
-ON e1.id=e2.managerId
-GROUP BY e1.id;
+FROM Employee AS e1
+JOIN (
+    SELECT managerId
+    FROM Employee
+    WHERE managerId IS NOT NULL        -- managerId가 존재하는 경우만 집계
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5               -- 부하 직원이 5명 이상인 경우만
+) AS sub
+ON e1.id = sub.managerId;              -- 매니저의 id와 sub쿼리의 managerId 매칭
