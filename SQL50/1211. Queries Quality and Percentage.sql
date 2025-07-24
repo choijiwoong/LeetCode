2398c45a29c916ed0ddcb60dee5974f7c4201e45
최지웅 <gogogi313@naver.com>
2025-07-24 11:15:18 +0900

    feat. CASE사용하기

--- SQL50/1211. Queries Quality and Percentage.sql
+++ SQL50/1211. Queries Quality and Percentage.sql
@@ -6,7 +6,7 @@
 # 결과 테이블은 query_name, quality, poor_query_percentage로 구성되어야 한다.
 # 모든 비율은 소수점 2자리까지 출력한다.
 # 순서상관없이 출력한다.
-SELECT q.query_name, ROUND(AVG(q.rating/q.position),2) as quality, ROUND(SUM(IF(q.rating<3, 1, 0))/COUNT(q.rating)*100,2) as poor_query_percentage
-FROM Queries as q
-GROUP BY q.query_name
-;
+SELECT query_name, ROUND(AVG(rating / position), 2) AS quality,
+ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*) * 100, 2) AS poor_query_percentage 
+FROM Queries 
+GROUP BY query_name;
