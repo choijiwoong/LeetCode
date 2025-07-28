43c39e65eabb62a0eb84408269afb851b4f26b8c
Choijiwoong <31060798+choijiwoong@users.noreply.github.com>
2025-07-28 21:23:26 +0900

    feat. 집계함수에서 GROUP 깨지는 버그 해결 필요

--- SQL50/1193. Monthly Transactions I.sql
+++ SQL50/1193. Monthly Transactions I.sql
@@ -1,6 +1,12 @@
 # Write your MySQL query statement below
+# state는 입출금 승인,실패를 나타낸다.
+# 각 나라와 도시 별로 거래건수와 총량, 승인된 거래의 개수와 총량을 나타내라.
+# Write your MySQL query statement below
 SELECT 
-    DATE_FORMAT(t.trans_date, '%Y-%m') as month
-    , t.country
+    DISTINCT DATE_FORMAT(t.trans_date, '%Y-%m') as month, 
+    t.country,
+    COUNT(*) as trans_count,
+    SUM(IF(t.state="approved", 1, 0)) as approved_count,
+    SUM(IF(t.state="approved", t.amount, 0)) as trans_total_amount
 FROM Transactions as t
 ORDER BY DATE_FORMAT(t.trans_date, '%Y-%m'), t.country
