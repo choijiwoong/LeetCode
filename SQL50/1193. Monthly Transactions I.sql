6b20ea399dfb9be1340e390911987d3dd5fa86bf
Choijiwoong <31060798+choijiwoong@users.noreply.github.com>
2025-07-28 21:39:30 +0900

    feat. country NULL값 처리 및 최적화 필요

--- SQL50/1193. Monthly Transactions I.sql
+++ SQL50/1193. Monthly Transactions I.sql
@@ -3,10 +3,18 @@
 # 각 나라와 도시 별로 거래건수와 총량, 승인된 거래의 개수와 총량을 나타내라.
 # Write your MySQL query statement below
 SELECT 
-    DISTINCT DATE_FORMAT(t.trans_date, '%Y-%m') as month, 
-    t.country,
+    t1.month,
+    t1.country,
     COUNT(*) as trans_count,
-    SUM(IF(t.state="approved", 1, 0)) as approved_count,
-    SUM(IF(t.state="approved", t.amount, 0)) as trans_total_amount
-FROM Transactions as t
-ORDER BY DATE_FORMAT(t.trans_date, '%Y-%m'), t.country
+    SUM(IF(t2.state="approved", 1, 0)) as approved_count,
+    SUM(t2.amount) as trans_total_amount,
+    SUM(IF(t2.state="approved", t2.amount, 0)) as approved_total_amount
+FROM(
+    SELECT 
+        DISTINCT DATE_FORMAT(t.trans_date, '%Y-%m') as month, 
+        t.country
+    FROM Transactions as t
+    ORDER BY DATE_FORMAT(t.trans_date, '%Y-%m'), t.country
+    ) as t1 JOIN Transactions as t2
+ON (t1.month = DATE_FORMAT(t2.trans_date, '%Y-%m') AND t1.country=t2.country)
+GROUP BY t1.month, t1.country
