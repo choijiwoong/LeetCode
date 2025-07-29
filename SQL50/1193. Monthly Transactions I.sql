79e4e93bb5d62d74a43d4c380cfd9a2d5a598909
Choijiwoong <31060798+choijiwoong@users.noreply.github.com>
2025-07-29 21:08:21 +0900

    debug. null값 처리 추가. 통계함수 디버깅 필요

--- SQL50/1193. Monthly Transactions I.sql
+++ SQL50/1193. Monthly Transactions I.sql
@@ -15,6 +15,6 @@ FROM(
         t.country
     FROM Transactions as t
     ORDER BY DATE_FORMAT(t.trans_date, '%Y-%m'), t.country
-    ) as t1 JOIN Transactions as t2
+    ) as t1 LEFT JOIN Transactions as t2
 ON (t1.month = DATE_FORMAT(t2.trans_date, '%Y-%m') AND t1.country=t2.country)
 GROUP BY t1.month, t1.country
